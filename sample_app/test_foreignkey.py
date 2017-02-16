d7db4677f4ed27f4bfdb8c2e9af2260868bb0ded
McKay Salisbury <mckay.salisbury@hulu.com>
2017-02-16 14:25:57 -0800

    Test fails

--- sample_app/test_foreignkey.py
+++ sample_app/test_foreignkey.py
@@ -1,6 +1,6 @@
 import pytest
-# from django.db.utils import IntegrityError
-# from django.db.backends import sqlite3
+from django.db.utils import IntegrityError
+from django.db.backends import sqlite3
 
 from sample_app.models import Referencer
 
@@ -8,6 +8,5 @@ from sample_app.models import Referencer
 @pytest.mark.django_db
 def test_break_foreign_key():
     """Tries to violate the foreign key"""
-    # Referenced.objects.create()
-    # with pytest.raises(IntegrityError):
-    Referencer.objects.create(fk_id=-1)
+    with pytest.raises(IntegrityError):
+        Referencer.objects.create(fk_id=-1)
